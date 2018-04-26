c6d475e26dde1aff081dbdff2802d112dc04c542
Jiachi Huang <brianhuang27@gmail.com>
2018-04-26 22:50:21 +0800

    Add big number adder and substractor.

--- week1/Karatsuba.cc
+++ week1/Karatsuba.cc
@@ -5,26 +5,27 @@
 #include <algorithm> 
 //int multiplier(int, int);
 std::string multiplier(std::string, std::string);
-void padZerosAtTheEnd(std::string, int);
+std::string bigNumberAdder(std::string, std::string);
+std::string bigNumberSubtracter(std::string, std::string);
+std::string padZerosAtTheEnd(std::string, int);
 
 int main(){
-    //std::string n1, n2;
-    //std::cin >> n1 >> n2;
-    //test
-    //std::cout<< multiplier("12","34") << " "<<12*34<<"\n";
+    //For basic testing
     std::cout<< multiplier("1234","5678") << " "<<1234*5678<<"\n";
-    std::cout<< multiplier("12345678","87654321") << " "<<12345678*87654321<<"\n";
-    //std::cout<< multiplier("9759571669751178","9751178519759571") << " "<<9759571669751178*9751178519759571<<"\n";
-    //std::cout << multiplier(n1, n2) << std::endl;
-    std::cout << padZerosAtTheEnd("KK",10) << "\n";
+    //For assignment
+    std::cout<< multiplier("3141592653589793238462643383279502884197169399375105820974944592",
+            "2718281828459045235360287471352662497757247093699959574966967627") <<"\n";
     return 1;
 }
 
-//Assume str1 and str2's length are always a power of 2
-unsigned int multiplier(std::string num1, std::string num2){
-    //std::cout << "input "<< num1 << " " << num2 << std::endl;
+/*  Use string type to record number.
+ *  Because int type will cause overflow.
+ */
+std::string multiplier(std::string num1, std::string num2){
+    
+    //  In case one of number is smaller than 10
     if (num1.size() < 2 || num2.size() < 2) {
-        return std::stoul(num1) * std::stoul(num2);
+        return std::to_string(std::stoul(num1) * std::stoul(num2));
     }
     int length =std::max(num1.size(), num2.size());
     int modLength = (length + 1) / 2;
@@ -38,23 +39,92 @@ unsigned int multiplier(std::string num1, std::string num2){
     if(num2_H == ""){
         num2_H = "0";
     }
-    /*std::cout<< "length:" << modLength<<" " <<num1 <<" "<<num2<<"\n";
-    std::cout << num1_H <<" "<<num1_L<<" " <<pow(10, (modLength))<<std::endl;
-    std::cout << num2_H <<" "<<num2_L<<std::endl;*/
-    //getchar();
     std::string part1 = multiplier(num1_H, num2_H);
     std::string part2 = multiplier(num1_L, num2_L);
-    std::string part3 = multiplier(std::to_string(stoi(num1_H) + stoi(num1_L)), std::to_string(stoi(num2_H)+stoi(num2_L))) - part1 - part2;
+    std::string part3 = multiplier(bigNumberAdder(num1_H, num1_L), 
+                                   bigNumberAdder(num2_H, num2_L));
+    part3 = bigNumberSubtracter(part3, part1);
+    part3 = bigNumberSubtracter(part3, part2);
+
     std::string ans = "";
-    ans = part1 + 
-    return pow(10, modLength*2) * part1 + 
-        pow(10, (modLength)) * part3 + 
-        part2;
+    ans = padZerosAtTheEnd(part1, modLength*2);
+    ans = bigNumberAdder(ans, padZerosAtTheEnd(part3, modLength));
+    ans = bigNumberAdder(ans, part2);
+    return ans;
 }
 
-void padZerosAtTheEnd(std::string& strNum, int length){
-    int nIter = 0;
-    while(nIter < length){
-        strNum = strNum + "0";
+std::string padZerosAtTheEnd(std::string strNum, int length){
+    std::string strAns = strNum;
+    while(--length>=0){
+        strAns = strAns + "0";
     }
+    return strAns;
+}
+
+std::string bigNumberAdder(std::string strNum1, std::string strNum2){
+    std::string strSum = "";
+    int nTemp = 0;
+    int nSum ,nCarry;
+    nSum = nCarry = 0;
+    for (unsigned int nIter = 0;nIter < std::max(strNum1.size(), strNum2.size());++nIter){
+        if (nIter < std::min(strNum1.size(), strNum2.size())){
+            const char a = strNum1[strNum1.size() - 1 - nIter];
+            const char b = strNum2[strNum2.size() - 1 - nIter];
+            nTemp = int(a) - '0' + int(b) - '0';
+        }
+        else {
+            nTemp = strNum1.size() > strNum2.size() ? 
+                int(strNum1[strNum1.size() - 1 - nIter]) - '0' : 
+                int(strNum2[strNum2.size() - 1 - nIter]) - '0';
+        }
+        nSum = (nTemp + nCarry) % 10;
+        nCarry = nTemp + nCarry >= 10? 1:0;
+        strSum = std::to_string(nSum) + strSum;
+    }
+    if(nCarry == 1){
+        strSum = "1" + strSum;
+    }
+    return strSum;
+}
+std::string bigNumberSubtracter(std::string strNum1, std::string strNum2){
+    std::string strSum = "";
+    int nTemp = 0;
+    int nSum ,nCarry;
+    int zeroCounter = -1;
+    nSum = nCarry = 0;
+    for (unsigned int nIter = 0;nIter < std::max(strNum1.size(), strNum2.size());++nIter){
+        if (nIter < std::min(strNum1.size(), strNum2.size())){
+            const char a = strNum1[strNum1.size() - 1 - nIter];
+            const char b = strNum2[strNum2.size() - 1 - nIter];
+            nTemp = int(a) - '0' -( int(b) - '0');
+        }
+        else {
+            nTemp = strNum1.size() > strNum2.size() ? 
+                int(strNum1[strNum1.size() - 1 - nIter]) - '0' : 
+                int(strNum2[strNum2.size() - 1 - nIter]) - '0';
+        }
+        if(nTemp < 0) {
+            nSum = nTemp + 10 + nCarry;
+            nCarry = -1;
+        }
+        else{
+            nSum = nTemp + nCarry;
+            nCarry = 0;
+        }
+        if(nSum < 0) {
+            nSum +=10;
+            nCarry -= 1;
+        }
+        strSum = std::to_string(nSum) + strSum;
+    }
+    for(int nIter = 0;nIter < strSum.size();++nIter){
+        if(strSum[nIter] == '0'){
+            zeroCounter = nIter;
+        }
+        else{
+            break;
+        }
+    }
+    strSum = strSum.substr(zeroCounter + 1, strSum.size() - (zeroCounter + 1));
+    return strSum;
 }
